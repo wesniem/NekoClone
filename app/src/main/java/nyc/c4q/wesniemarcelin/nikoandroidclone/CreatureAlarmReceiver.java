e2361af1a8c8d3365d280c6d23cd83a0b33baf26
wesniem <wesniem@yahoo.com>
2016-12-12 20:42:40 -0500

    Updated Database

---
+++ app/src/main/java/nyc/c4q/wesniemarcelin/nikoandroidclone/CreatureAlarmReceiver.java
@@ -0,0 +1,19 @@
+package nyc.c4q.wesniemarcelin.nikoandroidclone;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by wesniemarcelin on 12/4/16.
+ */
+
+public class CreatureAlarmReceiver extends BroadcastReceiver {
+    public static final int REQUEST_CODE = 12345;
+    public static final String ACTION = "nyc.c4q.notificationdemo.alarm";
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent i = new Intent(context, CreatureNotificationService.class);
+        context.startService(i);
+    }
+}
--- app/src/main/java/nyc/c4q/wesniemarcelin/nikoandroidclone/MainActivity.java
+++ app/src/main/java/nyc/c4q/wesniemarcelin/nikoandroidclone/MainActivity.java
@@ -1,7 +1,25 @@
 package nyc.c4q.wesniemarcelin.nikoandroidclone;
 
+import android.accessibilityservice.AccessibilityService;
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.media.MediaPlayer;
+import android.os.Vibrator;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Calendar;
+import java.util.Random;
+
+import nyc.c4q.wesniemarcelin.nikoandroidclone.model.Cat;
+
+import static nl.qbusict.cupboard.CupboardFactory.cupboard;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +27,76 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        showSimpleNotification();
+        showPendingIntentNotification();
+        scheduleAlarm();
     }
+    public void showSimpleNotification() {
+        int NOTIFICATION_ID = 555;
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.notification_icon_)
+                .setContentTitle("New Cat Alert!")
+                .setContentText("Click to add new cat to database");
+
+// Get the notification manager system service
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Setting a notification ID allows you to update the notification later on.
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+
+        vibratePhone();
+        catSound();
+    }
+    public void showPendingIntentNotification() {
+        int NOTIFICATION_ID = 555;
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.notification_icon_)
+                .setContentTitle("New Cat Alert!")
+                .setContentText("Click to add new cat to database");
+
+// Get the notification manager system service
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Setting a notification ID allows you to update the notification later on.
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+
+        vibratePhone();
+        catSound();
+    }
+    // ... call launchTestService() in the Activity onCreate()
+
+
+
+    private void vibratePhone() {
+        //Ensures that phone also vibrates with notification
+        Vibrator v = (Vibrator) this.getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+        // Vibrate for 500 milliseconds
+        v.vibrate(500);
+    }
+
+    private void catSound() {
+        MediaPlayer catSound = MediaPlayer.create(getApplicationContext(), R.raw.cat_meow);
+        catSound.start();
+    }
+
+    // Setup a recurring alarm every half hour
+    public void scheduleAlarm() {
+
+        // Construct an intent that will execute the AlarmReceiver
+        Intent intent = new Intent(getApplicationContext(), CreatureAlarmReceiver.class);
+
+        // Create a PendingIntent to be triggered when the alarm goes off
+        final PendingIntent pendingIntent = PendingIntent.getBroadcast(this, CreatureAlarmReceiver.REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        long firstMillis = System.currentTimeMillis(); // alarm is set right away
+
+        AlarmManager alarm = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+
+        // First parameter is the type: ELAPSED_REALTIME, ELAPSED_REALTIME_WAKEUP, RTC_WAKEUP
+        // Interval can be INTERVAL_FIFTEEN_MINUTES, INTERVAL_HALF_HOUR, INTERVAL_HOUR, INTERVAL_DAY
+        alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, firstMillis, (60000), pendingIntent);
+    }
+
 }
